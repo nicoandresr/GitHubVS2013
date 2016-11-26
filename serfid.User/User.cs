e11a97b4cbd8858212b0d2f6c8b1771ecc73d26b
nicoandresr <nicoandres.rodriguez@gmail.com>
2016-11-26 06:40:00 -0500

    Add users.cs an update .gitignore for exclude these files.

---
+++ serfid.User/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using serfid.Interfaces.DataAccess;
+using serfid.Interfaces.Enumerations;
+using serfid.Interfaces.System;
+using serfid.Interfaces.User;
+using serfid.Interfaces.ValueObjects;
+
+namespace serfid.User
+{
+    public class User : IUser
+    {
+        #region Attributes
+
+        private readonly ISerfidDataAccess _serfidDataAccess;
+
+        #endregion
+
+        #region Constructor
+
+        public User(ISerfidDataAccess serfidDataAccess)
+        {
+            _serfidDataAccess = serfidDataAccess;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public ModuleStatus Start()
+        {
+            Console.WriteLine("User module started!");
+            return ModuleStatus.success;
+        }
+
+        public List<ReadingInfo> GetReadings(PagingInfo parameters)
+        {
+            return _serfidDataAccess.GetReadings(parameters);
+        }
+
+        #endregion
+    }
+}
