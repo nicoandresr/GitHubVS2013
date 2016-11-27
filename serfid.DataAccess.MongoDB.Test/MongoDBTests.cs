ebc9474cbc5b6b96a3571c6e9f21584eb3db8493
nicoandresr <nicoandres.rodriguez@gmail.com>
2016-11-27 15:14:32 -0500

    Integration with mongodb

---
+++ serfid.DataAccess.MongoDB.Test/MongoDBTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using serfid.Interfaces.ValueObjects;
+using System.Collections.Generic;
+using serfid.Interfaces.System;
+
+namespace serfid.DataAccess.MongoDB.Test
+{
+    [TestClass]
+    public class MongoDBTests
+    {
+        #region Fields
+
+        private MongoDataAccess _mongoDA;
+
+        #endregion
+
+        #region Test initialize
+
+        [TestInitialize]
+        public void ConnectWithMongoServer()
+        {
+            // Arrange
+            _mongoDA = new MongoDataAccess();
+
+            // Act
+
+            // Assert
+            Assert.AreNotEqual(null, _mongoDA);
+        }
+
+        #endregion
+
+        #region Test cleanup
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _mongoDA = null;
+        }
+
+        #endregion
+
+        #region Tests
+
+        [TestMethod]
+        public void SaveReading()
+        {
+            // Arrange
+            ReadingInfo info = new ReadingInfo
+            {
+                Reader = "R2589",
+                Tag = Guid.NewGuid().ToString().Replace("-", "").ToUpper().Substring(0, 19),
+                ReadingDateTime = DateTime.Now
+            };
+
+            // Act
+            _mongoDA.SaveReading(info);
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void GetReadings()
+        {
+            // Arrange
+            List<ReadingInfo> readings = null;
+
+            // Act
+            readings = _mongoDA.GetReadings(new PagingInfo());
+
+            // Assert
+            Assert.AreNotEqual(null, readings);
+        }
+
+        #endregion
+    }
+}
---
+++ serfid.DataAccess.MongoDB/MongoDataAccess.cs
@@ -0,0 +1,48 @@
+﻿using serfid.Interfaces.DataAccess;
+using System.Linq;
+using System.Collections.Generic;
+using serfid.Interfaces.System;
+using serfid.Interfaces.ValueObjects;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace serfid.DataAccess.MongoDB
+{
+    public class MongoDataAccess : ISerfidDataAccess
+    {
+        private readonly IMongoDatabase _db;
+
+        public MongoDataAccess()
+        {
+            MongoClient mongoClient = new MongoClient("mongodb://nico:'S3rf1d'@ds061246.mlab.com:61246/serfid-db");
+            _db = mongoClient.GetDatabase("serfid-db");
+        }
+
+        public List<ReadingInfo> GetReadings(PagingInfo parameters)
+        {
+            List<ReadingInfo> result = new List<ReadingInfo>();
+            IMongoCollection<BsonDocument> collection = _db.GetCollection<BsonDocument>("readings");
+            BsonDocument filter = new BsonDocument();
+            IFindFluent<BsonDocument, BsonDocument> documents = collection.Find(filter);
+            result = documents.ToList().Select(d => new ReadingInfo
+            {
+                Reader = d.GetValue("reader").ToString(),
+                Tag = d.GetValue("tag").ToString(),
+                ReadingDateTime = d.GetValue("date").ToUniversalTime()
+            }).ToList();
+            return result;
+        }
+
+        public void SaveReading(ReadingInfo readingInfo)
+        {
+            IMongoCollection<BsonDocument> collection = _db.GetCollection<BsonDocument>("readings");
+            BsonDocument document = new BsonDocument
+            {
+                { "reader", readingInfo.Reader },
+                { "tag", readingInfo.Tag },
+                { "date", readingInfo.ReadingDateTime }
+            };
+            collection.InsertOne(document);
+        }
+    }
+}
