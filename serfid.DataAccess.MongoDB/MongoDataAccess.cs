b48960301814acb589ad7955ac5c7cb1af10197e
nicoandresr <nicoandres.rodriguez@gmail.com>
2016-11-30 23:43:02 -0500

    Fix unit test "getReadings" of module MongoDB

--- serfid.DataAccess.MongoDB/MongoDataAccess.cs
+++ serfid.DataAccess.MongoDB/MongoDataAccess.cs
@@ -23,17 +23,13 @@ namespace serfid.DataAccess.MongoDB
         {
             List<ReadingLog> result = new List<ReadingLog>();
             IMongoCollection<BsonDocument> collection = _db.GetCollection<BsonDocument>("readings");
-            IMongoCollection<BsonDocument> devicesCollection = _db.GetCollection<BsonDocument>("devices");
             BsonDocument filter = new BsonDocument();
             IFindFluent<BsonDocument, BsonDocument> documents = collection.Find(filter);
-            IFindFluent<BsonDocument, BsonDocument> devices = devicesCollection.Find(filter);
             result = documents.ToList().Select(d => new ReadingLog
             {
                 Reader = d.GetValue("reader").ToString(),
                 Tag = d.GetValue("tag").ToString(),
                 ReadingDateTime = d.GetValue("date").ToUniversalTime(),
-                Device = devices.ToList().FirstOrDefault(x => x.GetValue("tag") == d.GetValue("tag")).GetValue("device").ToString(),
-                ImageUrl = devices.ToList().FirstOrDefault(x => x.GetValue("tag") == d.GetValue("tag")).GetValue("imageUrl").ToString(),
             }).ToList();
             return result;
         }
